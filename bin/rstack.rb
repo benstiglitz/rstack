69309d13eda61bdadf6cbd1cf01e0380b6fde4e4
Benjamin Stiglitz <ben@tanjero.com>
2009-04-13 09:56:24 -0400

    Bootstrapping time.
    The REPL runs out of the prelude now. The prelude is executed with ARGV
    on the stack. Exception handling isn't working properly anymore; there
    needs to be a retry handler to rerun the REPL.

--- bin/rstack.rb
+++ bin/rstack.rb
@@ -11,20 +11,17 @@ if __FILE__ == $0
     include RStack
     vm = VM.new
 
-    # read prelude
-    unless ARGV.include?('--no-prelude')
-	prelude_path = File.join(File.dirname(File.dirname(__FILE__)), 'lib', 'prelude.rs')
-	vm.exec(Optimizer.optimize(Lexer.lex(File.read(prelude_path))))
-    end
+    # push ARGV onto the stack
+    vm.exec [:cons, ARGV.dup]
 
-    # start REPL
-    while input = prompt
-	begin
-	    vm.exec(Optimizer.optimize(Lexer.lex(input)))
-	    vm.exec([:stack_print])
-	rescue Exception => e
-	    puts "Caught \"#{e}\"\n"
-	    pp e.backtrace if $DEBUG
-	end
+    # read prelude
+    prelude_path = File.join(File.dirname(File.dirname(__FILE__)), 'lib', 'prelude.rs')
+    prelude = Optimizer.optimize(Lexer.lex(File.read(prelude_path)))
+
+    begin
+	vm.exec(prelude)
+    rescue Exception => e
+	puts "Caught \"#{e}\"\n"
+	pp e.backtrace if $DEBUG
     end
 end
--- lib/prelude.rs
+++ lib/prelude.rs
@@ -4,4 +4,21 @@
 :lex [ Lexer :lex 1 #f native_call ] ;
 :optimize [ Optimizer :optimize 1 #f native_call ] ;
 :compile [ lex optimize ] ;
+
+:read [ STDIN :gets 0 #f native_call ] ;
 :eval [ compile call ] ;
+
+:print [ Kernel :print 1 #f native_call drop ] ;
+
+:loop [ 9999999999999 :times 0 #t native_call ] ;
+
+:1rep [ "rstack> " print read eval stack_print ] ;
+:repl [ [ 1rep ] loop ] ;
+
+"--no-prelude" swap :include? 1 #f native_call [ ] [
+    ( start prelude )
+
+    ( end prelude )
+] if
+
+repl
